686b4643dcc04d196b675b88ba8a8e04144e8dff
Teech <Teecha003@gmail.com>
2024-05-02 14:52:38 +0700

    input number

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,28 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        Scanner sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        if (num >= 80) {
+            System.out.println("grade A");
+            System.out.print("score"+num);
+            }
+        else if (num >= 70) {
+            System.out.println("grade B");
+            System.out.print("score"+num);
         }
+        else if (num >= 60) {
+            System.out.println("grade C");
+            System.out.print("score"+num);
+        }
+        else if (num >= 50) {
+            System.out.println("grade D");
+            System.out.print("score"+num);
+        }
+        else  if (num <= 49) {
+            System.out.println("grade F");
+            System.out.print("score"+num);
+        }
+
+    }
     }
